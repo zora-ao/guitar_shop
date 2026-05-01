0282997c03ae215e493011e11afb05e32e826757
Carl Jasper Ramos <zoraao79@gmail.com>
2026-05-01 17:38:55 +0800

    last

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 // src/utils/api.ts
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
+console.log("Current API URL:", API_BASE_URL);
+
 export const getApiUrl = (endpoint: string) => {
     const cleanEndpoint = endpoint.startsWith('/') ? endpoint.slice(1) : endpoint;
     return `${API_BASE_URL}/${cleanEndpoint}`;
