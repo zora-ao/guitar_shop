b1da67b98143f9268f924835fcc1bb1ce9ed2b2a
Carl Jasper Ramos <zoraao79@gmail.com>
2026-05-24 22:23:45 +0800

    fiz a little error

--- src/components/collection/FilterSidebar.tsx
+++ src/components/collection/FilterSidebar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { ChevronDown, Filter, X } from 'lucide-react';
 
